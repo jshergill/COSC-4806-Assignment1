31bea7e61e8423bf2ba5439284ab28e50705a2ba
jshergill <jshergill@algomau.ca>
2025-05-18 03:13:29 +0000

    Added if statements to validate
    Check failed attempts.

--- Validate.php
+++ Validate.php
@@ -1,4 +1,7 @@
 <?php 
+
+session_start(); 
+
 $valid_username = 'jshergill';
 $valid_password = '123123';
 
@@ -6,7 +9,14 @@ $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 
 if ($username == $valid_username && $password == $valid_password){
-    echo "Login successful";}
-    else{
-    echo "Login failed";}
-?>
+    echo "Login successful";
+} else {
+    
+    if (!isset($_SESSION['Failed_attempts'])){
+        $_SESSION['Failed_attempts'] = 1;
+    } else {
+        $_SESSION['Failed_attempts'] += 1;
+    }
+    echo "Login failed. Attempt no.: " . $_SESSION['Failed_attempts'];
+ }
+?> 
